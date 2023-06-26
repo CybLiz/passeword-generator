c85f1b3516ce75078fa8b9f2c6b93d4274b24d56
CybLiz <liza.bahloul@gmail.com>
2023-06-26 09:00:19 +0200

    brief fini

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
 
 const button = document.getElementById('button')
 const password = document.getElementById('password')
+let sizeSelector = document.getElementById('sizeSelector')
 
-// au click sur le boutton , tu ajoute ce qu'il y a dans ma div avec lId password, la fonction générée password
+// au click sur le boutton , tu ajoute ce qu'il y a dans ma div avec l'Id password, la fonction générée password
 // () =>  : fonction fléchée remplace le mot "function"
 button.addEventListener('click', () => {
 	password.innerHTML = generatePassword(20)
@@ -18,13 +19,18 @@ function generatePassword(size) {
 	// charAt en lui passant en paramétre (Math.random() *  c.length)
 	for (let i = 0; i < size; i++) {
 		passwordgenerated += c.charAt(Math.random() * c.length);
+		// sizeSelector = generatePassword(size);
 
 	}
 	return passwordgenerated;
+
 }
 
 
-// méthode 2
+// Add a selector to define the size of the password to generate.
+
+
+// méthode 2 il faut remplacer la 2éme div par un input
 
 
 // const c =  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmopqstuvwxyz12345670§?!/<>=+-@#ù%^:.é"'
@@ -41,3 +47,6 @@ function generatePassword(size) {
 // 	// console.log(password)
 // 	input.value = password
 // }
+
+
+// Add a selector to define the size of the password to generate.
